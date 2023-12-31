0d9fafb3ecf5aea5f26f5c5d1dbe0a68532f6684
21Cash <21ThousandProof@gmail.com>
2023-12-31 23:06:09 +0530

    Added board

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Game from "./pages/Game";
 
 function App() {
   return (
     <Router>
-    <div className="pt-20">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+      <div className="pt-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Game" element={<Game />} />
+        </Routes>
+      </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Game.jsx
@@ -0,0 +1,122 @@
+import React, { forwardRef, useEffect, useRef, useState, useMemo } from "react";
+import { Chess } from "chess.js";
+import { Chessboard } from "react-chessboard";
+
+const buttonStyle = {
+  cursor: "pointer",
+  padding: "10px 20px",
+  margin: "10px 10px 0px 0px",
+  borderRadius: "6px",
+  backgroundColor: "#f0d9b5",
+  border: "none",
+  boxShadow: "0 2px 5px rgba(0, 0, 0, 0.5)",
+};
+
+const inputStyle = {
+  padding: "10px 20px",
+  margin: "10px 0 10px 0",
+  borderRadius: "6px",
+  border: "none",
+  boxShadow: "0 2px 5px rgba(0, 0, 0, 0.5)",
+};
+
+const boardWrapper = {
+  width: `70vw`,
+  maxWidth: "70vh",
+  margin: "3rem auto",
+};
+
+const Game = () => {
+  const [game, setGame] = useState(new Chess());
+  const [currentTimeout, setCurrentTimeout] = useState();
+  const chessboardRef = useRef(null);
+
+  function safeGameMutate(modify) {
+    setGame((g) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
+  }
+
+  function makeRandomMove() {
+    const possibleMoves = game.moves();
+
+    // exit if the game is over
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
+      return;
+
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    safeGameMutate((game) => {
+      game.move(possibleMoves[randomIndex]);
+    });
+  }
+
+  function onDrop(sourceSquare, targetSquare, piece) {
+    const gameCopy = { ...game };
+    const move = gameCopy.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: piece[1].toLowerCase() ?? "q",
+    });
+    setGame(gameCopy);
+
+    // illegal move
+    if (move === null) return false;
+
+    // store timeout so it can be cleared on undo/reset so computer doesn't execute move
+    const newTimeout = setTimeout(makeRandomMove, 2000);
+    setCurrentTimeout(newTimeout);
+    return true;
+  }
+
+  return (
+    <div style={boardWrapper}>
+      <Chessboard
+        id="PremovesEnabled"
+        arePremovesAllowed={true}
+        position={game.fen()}
+        isDraggablePiece={({ piece }) => piece[0] === "w"}
+        onPieceDrop={onDrop}
+        customBoardStyle={{
+          borderRadius: "4px",
+          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)",
+        }}
+        ref={chessboardRef}
+        allowDragOutsideBoard={false}
+      />
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          safeGameMutate((game) => {
+            game.reset();
+          });
+          // clear premove queue
+          chessboardRef.current?.clearPremoves();
+          // stop any current timeouts
+          clearTimeout(currentTimeout);
+        }}
+      >
+        reset
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          // undo twice to undo computer move too
+          safeGameMutate((game) => {
+            game.undo();
+            game.undo();
+          });
+          // clear premove queue
+          chessboardRef.current?.clearPremoves();
+          // stop any current timeouts
+          clearTimeout(currentTimeout);
+        }}
+      >
+        undo
+      </button>
+    </div>
+  );
+};
+
+export default Game;
